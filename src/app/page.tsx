cbd504b5c9fa87a0e21d9350526d2c19e85a11ee
emad <arabanemad@gmail.com>
2025-06-13 12:42:55 +0330

    for test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
@@ -10,7 +11,7 @@ const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        {/* <FloatingNav navItems={navItems} /> */}
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects slides={projectData} />
