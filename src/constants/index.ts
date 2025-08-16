a5929910660cff4383be1abd33a09f30f4c367e8
emad <arabanemad@gmail.com>
2025-08-16 14:30:28 +0330

    fixed name myPhoto

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -26,7 +26,7 @@ export const gridItems = [
     imgClassName: "w-full h-full object-contain",
     titleClassName: "",
     style: { objectFit: "contain", borderRadius: "50%" },
-    img: "/emad(1).png",
+    img: "/Emad(1).png",
     video: "",
     spareImg: "",
   },
